5023adbadc48f9bf643ad894b9aa257e9c5c1e78
yangwen0228 <yangwen0228@foxmail.com>
2018-12-18 22:06:20 +0800

    exec path

---
+++ layers/+tool/exec-path/packages.el
@@ -0,0 +1,31 @@
+;;; packages.el --- exec-path layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2018 WEN YANG & Contributors
+;;
+;; Author: WEN YANG <steven@WENdeMacBook-Pro.local>
+;; URL: https://github.com/yangwen0228/.spacemacs.d
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; exec-path
+
+;;; Code:
+
+(setq exec-path-packages
+      '(exec-path-from-shell))
+
+(defun exec-path/post-init-exec-path-from-shell ()
+  (cond
+   ((spacemacs/system-is-mac) (add-to-list 'exec-path "/usr/local/Cellar/node/11.4.0/bin"))
+   ;; ((spacemacs/system-is-mswindows) (add-to-list 'exec-path ""))
+   ))
+
+
+
+
+
+;;; packages.el ends here
